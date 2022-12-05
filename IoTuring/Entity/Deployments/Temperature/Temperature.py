c734db78b115d2bdef305f7e9270eb2f4312bf10
Riccardo Briccola <riccardo.briccola.dev@gmail.com>
2022-12-05 13:49:20 +0100

    Added support for Extra atttributes. Extra attributes for Temperature

--- IoTuring/Entity/Deployments/Temperature/Temperature.py
+++ IoTuring/Entity/Deployments/Temperature/Temperature.py
@@ -3,6 +3,9 @@ from IoTuring.Entity.Entity import Entity
 from IoTuring.Entity.EntityData import EntitySensor
 
 KEY_SENSOR_FORMAT = "{}"
+FALLBACK_PACKAGE_LABEL = "package_{}"
+FALLBACK_SENSOR_LABEL = "sensor_{}"
+
 
 class Temperature(Entity):
     NAME = "Temperature"
@@ -12,25 +15,34 @@ class Temperature(Entity):
     def Initialize(self):
         self.registeredPackages = []
         sensors = psutil.sensors_temperatures()
-        for device, data in sensors.items():       
+        for device, data in sensors.items():
             package = psutilTemperaturePackage(device, data)
-            if package.hasCurrent():    
+            if package.hasCurrent():
                 self.registeredPackages.append(package.getLabel())
-                self.RegisterEntitySensor(EntitySensor(self, self.packageNameToEntitySensorKey(package.getLabel())))
- 
+                # TODO supportsExtraAttributes only on Linux
+                self.RegisterEntitySensor(EntitySensor(
+                    self, self.packageNameToEntitySensorKey(package.getLabel()), supportsExtraAttributes=True))
+
     def Update(self):
         sensors = psutil.sensors_temperatures()
-        for device, data in sensors.items():
-            package = psutilTemperaturePackage(device, data)     
+        index = 1
+        for pkgName, data in sensors.items():
+            if pkgName == None or pkgName == "":
+                pkgName = FALLBACK_PACKAGE_LABEL.format(index)
+            package = psutilTemperaturePackage(pkgName, data)
             if package.getLabel() in self.registeredPackages:
                 # Set main value = current of the package
-                self.SetEntitySensorValue(self.packageNameToEntitySensorKey(package.getLabel()), package.getCurrent())
-                # TODO set extra values
-    
+                # TODO Temperature in ValueFormatter
+                self.SetEntitySensorValue(self.packageNameToEntitySensorKey(
+                    package.getLabel()), package.getCurrent())
+                self.SetEntitySensorExtraAttributes(self.packageNameToEntitySensorKey(
+                    package.getLabel()), package.getAttributesDict())
+            index += 1
+
     def packageNameToEntitySensorKey(self, packageName):
         return KEY_SENSOR_FORMAT.format(packageName)
-            
-    
+
+
 class psutilTemperaturePackage():
     def __init__(self, packageName, packageData) -> None:
         """ packageData is the value of the the dict returned by psutil.sensors_temperatures() """
@@ -38,13 +50,13 @@ class psutilTemperaturePackage():
         self.sensors = []
         for sensor in packageData:
             self.sensors.append(psutilTemperatureSensor(sensor))
-            
+
     def getLabel(self) -> str:
         return self.packageName
-            
+
     def getSensors(self) -> list:
         return self.sensors.copy()
-        
+
     # Package stats strategies here: my choose is to return always the highest among the temperatures, critical=True if a sensor has critical=True
     def getCurrent(self):
         """ Returns highest current temperature among this package sensors. None if any sensor has that data. """
@@ -53,7 +65,7 @@ class psutilTemperaturePackage():
             if sensor.hasCurrent() and (highest == None or highest < sensor.getCurrent()):
                 highest = sensor.getCurrent()
         return highest
-    
+
     def getHighest(self):
         """ Returns highest current temperature among this package sensors. None if any sensor has that data. """
         highest = None
@@ -61,36 +73,53 @@ class psutilTemperaturePackage():
             if sensor.hasHighest() and (highest == None or highest < sensor.getHighest()):
                 highest = sensor.getHighest()
         return highest
-    
+
     def getCritical(self):
         """ Returns True if at least a package is critical. False otherwise """
         for sensor in self.getSensors():
             if sensor.hasCritical() and sensor.getCritical():
                 return True
         return False
-        
+
     def hasCurrent(self):
         """ True if at least a sensor of the package has the current property """
         for sensor in self.sensors:
             if sensor.hasCurrent():
                 return True
         return False
-        
+
     def hasHighest(self):
         """ True if at least a sensor of the package has the highest property """
         for sensor in self.sensors:
             if sensor.hasHighest():
                 return True
         return False
-        
+
     def hasCritical(self):
         """ True if at least a sensor of the package has the critical property """
         for sensor in self.sensors:
             if sensor.hasCritical():
                 return True
         return False
-    
-    
+
+    def getAttributesDict(self):
+        attributes = {}
+        for index, sensor in enumerate(self.getSensors()):
+            if sensor.hasLabel():
+                label = sensor.getLabel()
+            else:
+                label = FALLBACK_SENSOR_LABEL.format(index)
+            if sensor.hasCurrent():
+                attributes[label +
+                           " - Current"] = str(sensor.getCurrent()) + "°C"
+            if sensor.hasHighest():
+                attributes[label +
+                           " - Highest"] = str(sensor.getHighest()) + "°C"
+            if sensor.hasCritical():
+                attributes[label + " - Critical"] = str(sensor.getCritical())
+        return attributes
+
+
 class psutilTemperatureSensor():
     def __init__(self, sensorData) -> None:
         """ sensorData is an element from the list which is the value of the the dict returned by psutil.sensors_temperatures() """
@@ -98,31 +127,31 @@ class psutilTemperatureSensor():
         self.current = sensorData[1]
         self.highest = sensorData[2]
         self.critical = sensorData[3]
-    
+
     def getCurrent(self):
         return self.current
-    
+
     def getLabel(self) -> str:
         return self.label
-    
+
     def getHighest(self):
         return self.highest
-    
+
     def getCritical(self):
         return self.critical
-    
+
     def hasLabel(self):
         """ True if a label is set for this sensor """
         return not self.label == None and not self.label.strip() == ""
-    
+
     def hasCurrent(self):
         """ True if a current is set for this sensor """
         return not self.current == None
-    
+
     def hasHighest(self):
         """ True if a highest is set for this sensor """
         return not self.highest == None
-    
+
     def hasCritical(self):
         """ True if a critical is set for this sensor """
         return not self.critical == None
--- IoTuring/Entity/EntityData.py
+++ IoTuring/Entity/EntityData.py
@@ -1,5 +1,6 @@
 from IoTuring.Logger.LogObject import LogObject
 
+# EntitySensor extra attribute aren't read from all the warehouses 
 
 class EntityData(LogObject):
 
@@ -20,30 +21,43 @@ class EntityData(LogObject):
 
 class EntitySensor(EntityData):
 
-    def __init__(self, entity, key):
+    def __init__(self, entity, key, supportsExtraAttributes = False):
         EntityData.__init__(self, entity, key)
+        self.supportsExtraAttributes = supportsExtraAttributes
         self.value = None
         self.extraAttributes = None
+
+    def DoesSupportExtraAttributes(self):
+        return self.supportsExtraAttributes
+
+    def GetValue(self):
+        return self.value
 
     def SetValue(self, value):
         value = str(value)
         self.Log(self.LOG_DEBUG, "Set to " + value)
         self.value = value
         return self.value
-    
-    def SetExtraAttributes(self, _dict):
-        # a dict with name: value that is compatible only with certain warehouses
-        self.extraAttributes = _dict
-
-    def GetValue(self):
-        return self.value
-
-    def GetExtraAttributes(self):
-        return self.extraAttributes
 
     def HasValue(self):
         """ True if self.value isn't empty """
         return self.value is not None
+
+    def GetExtraAttributes(self):
+        if self.supportsExtraAttributes == False:
+            raise Exception("This entity sensor does not support extra attributes. Please specify it when initializing the sensor.")
+        return self.extraAttributes
+    
+    def HasExtraAttributes(self):
+        """ True if self.extraAttributes isn't empty """
+        return self.extraAttributes is not None
+    
+    def SetExtraAttributes(self, _dict):
+        # a dict with (attribute_name: value) that is compatible only with certain warehouses
+        if self.supportsExtraAttributes == False:
+            raise Exception("This entity sensor does not support extra attributes. Please specify it when initializing the sensor.")
+        self.extraAttributes = _dict
+
 
 
 class EntityCommand(EntityData):
