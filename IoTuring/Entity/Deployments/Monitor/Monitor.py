98a1cb77bcc4bcce002113fbfa567ab4a7837049
Riccardo Briccola <12238652+richibrics@users.noreply.github.com>
2023-01-10 18:59:59 +0100

    Revert "Monitor: add support to change monitor mode for Windows"

--- IoTuring/Entity/Deployments/Monitor/Monitor.py
+++ IoTuring/Entity/Deployments/Monitor/Monitor.py
@@ -8,10 +8,6 @@ from IoTuring.Entity.EntityData import EntityCommand
 
 KEY_TURN_ALL_OFF = 'turn_all_off'
 KEY_TURN_ALL_ON = 'turn_all_on'
-KEY_INTERNAL_MONITOR = "internal"
-KEY_EXTERNAL_MONITOR = "external"
-KEY_EXTEND_MONITOR = "extend"
-KEY_CLONE_MONITOR = "clone"
 
 
 class Monitor(Entity):
@@ -29,16 +25,6 @@ class Monitor(Entity):
                 self, KEY_TURN_ALL_OFF, self.CallbackTurnAllOff))
             self.RegisterEntityCommand(EntityCommand(
                 self, KEY_TURN_ALL_ON, self.CallbackTurnAllOn))
-            sr = sys_os.environ.get('SystemRoot')
-            if self.os == 'Windows' and sys_os.path.exists('{}\System32\DisplaySwitch.exe'.format(sr)):
-                self.RegisterEntityCommand(EntityCommand(
-                    self, KEY_INTERNAL_MONITOR, self.CallbackInternalMonitor))
-                self.RegisterEntityCommand(EntityCommand(
-                    self, KEY_EXTERNAL_MONITOR, self.CallbackExternalMonitor))
-                self.RegisterEntityCommand(EntityCommand(
-                    self, KEY_EXTEND_MONITOR, self.CallbackExtendMonitor))
-                self.RegisterEntityCommand(EntityCommand(
-                    self, KEY_CLONE_MONITOR, self.CallbackCloneMonitor))
 
     def CallbackTurnAllOff(self, message):
         if self.os == 'Windows':
@@ -57,27 +43,3 @@ class Monitor(Entity):
             if sys_os.environ.get('DISPLAY'):
                 command = 'xset dpms force on'
                 subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-
-    def CallbackInternalMonitor(self, message):
-        if self.os == 'Windows':
-            sr = sys_os.environ.get('SystemRoot')
-            command = '{}\System32\DisplaySwitch.exe /internal'.format(sr)
-            subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-
-    def CallbackExternalMonitor(self, message):
-        if self.os == 'Windows':
-            sr = sys_os.environ.get('SystemRoot')
-            command = '{}\System32\DisplaySwitch.exe /external'.format(sr)
-            subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-
-    def CallbackExtendMonitor(self, message):
-        if self.os == 'Windows':
-            sr = sys_os.environ.get('SystemRoot')
-            command = '{}\System32\DisplaySwitch.exe /extend'.format(sr)
-            subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-
-    def CallbackCloneMonitor(self, message):
-        if self.os == 'Windows':
-            sr = sys_os.environ.get('SystemRoot')
-            command = '{}\System32\DisplaySwitch.exe /clone'.format(sr)
-            subprocess.Popen(command.split(), stdout=subprocess.PIPE)
