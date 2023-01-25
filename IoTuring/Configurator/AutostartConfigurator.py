685ab9263f0440af60bed0aa38032d258045589e
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2023-01-25 16:51:36 +0100

    systemwide option

--- IoTuring/Configurator/AutostartConfigurator.py
+++ IoTuring/Configurator/AutostartConfigurator.py
@@ -8,18 +8,24 @@ class AutostartConfigurator():
         selection = False
         while selection is False:
             print("\nConfiguring autostart...")
-            print("1 - Enable autostart")
-            print("2 - Disable autostart")
+            print("1 - Enable autostart [system-wide]")
+            print("2 - Disable autostart [system-wide]")
+            print("3 - Enable autostart [user-wide]")
+            print("4 - Disable autostart [user-wide]")
             selection = input("Select an option: ")
             
             if selection == "1":
-                AutostartConfigurator.enable()
+                AutostartConfigurator.enable(True)
             elif selection == "2":
-                AutostartConfigurator.disable()
+                AutostartConfigurator.disable(True)
+            elif selection == "3":
+                AutostartConfigurator.enable(False)
+            elif selection == "4":
+                AutostartConfigurator.disable(False)
             else:
                 selection = False
         
-    def enable():
+    def enable(system_wide):
         try: 
             app_name = App.getName()
             script_location = f"-m {app_name}"
@@ -28,6 +34,7 @@ class AutostartConfigurator():
             autostarter.add(
                 script_location,
                 identifier=app_name,
+                system_wide=system_wide,
                 interpreter=interpreter
             )
             print("Autostart enabled successfully")
@@ -35,10 +42,10 @@ class AutostartConfigurator():
             print("Autostart could not be enabled")
             print("Error: " + str(e))
         
-    def disable():
+    def disable(system_wide):
         try:
             app_name = App.getName()
-            result = autostarter.remove(app_name)
+            result = autostarter.remove(app_name, system_wide=system_wide)
             
             if result is True: 
                 print("Autostart disabled successfully")
