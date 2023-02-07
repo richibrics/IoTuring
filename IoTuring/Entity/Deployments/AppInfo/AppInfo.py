474965518b75646694838a07d55fd74d1ddee1fb
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2023-02-07 12:05:25 +0100

    Extra attribute for AppInfo Version
    Entity update called also on start (without waiting for the timeout the first time)

--- IoTuring/Entity/Deployments/AppInfo/AppInfo.py
+++ IoTuring/Entity/Deployments/AppInfo/AppInfo.py
@@ -8,36 +8,38 @@ KEY_VERSION = 'version'
 KEY_UPDATE = 'update'
 PYPI_URL = 'https://pypi.org/pypi/ioturing/json'
 
+EXTRA_ATTRIBUTE_LATEST = 'Latest version'
+
 class AppInfo(Entity):
     NAME = "AppInfo"
 
     def Initialize(self):
         self.RegisterEntitySensor(EntitySensor(self, KEY_NAME))
         self.RegisterEntitySensor(EntitySensor(self, KEY_VERSION))
-        self.RegisterEntitySensor(EntitySensor(self, KEY_UPDATE))
+        self.RegisterEntitySensor(EntitySensor(self, KEY_UPDATE, supportsExtraAttributes=True))
 
     def PostInitialize(self):
         self.SetEntitySensorValue(KEY_NAME, App.getName())
         self.SetEntitySensorValue(KEY_VERSION, App.getVersion())
-        if self.GetUpdateInformation():
-            self.SetEntitySensorValue(
-                KEY_UPDATE, True)
-        else:
-            self.SetEntitySensorValue(
-                KEY_UPDATE, False)
-        self.updateTimeout = 600
+        self.SetUpdateTimeout(600)
 
     def Update(self):
-        if self.GetUpdateInformation():
-            self.SetEntitySensorValue(
-                KEY_UPDATE, True)
-        else:
+        # VERSION UPDATE CHECK
+        new_version = self.GetUpdateInformation()
+        if not new_version:
             self.SetEntitySensorValue(
                 KEY_UPDATE, False)
+            self.SetEntitySensorExtraAttributes(KEY_UPDATE, {EXTRA_ATTRIBUTE_LATEST: App.getVersion()})
+        else:
+            self.SetEntitySensorValue(
+                KEY_UPDATE, True)
+            self.SetEntitySensorExtraAttributes(KEY_UPDATE, {EXTRA_ATTRIBUTE_LATEST: new_version})
 
     def GetUpdateInformation(self):
         """
         Get the update information of IoTuring
+        Returns False if no update is available
+        Otherwise returns the latest version (could be set as extra attribute)
         """
         latest = ""
         res = requests.get(PYPI_URL)
@@ -45,9 +47,11 @@ class AppInfo(Entity):
             info = res.json().get("info", "")
             if len(info) >= 1:
                 latest = info.get("version", "")
-        current = ''.join([i for i in App.getVersion() if i.isdigit()])
         if len(latest) >= 1:
-            latest = ''.join([i for i in latest if i.isdigit()])
-            if int(latest) > int(current):
-                return True
+            if versionToInt(latest) > versionToInt(App.getVersion()):
+                return latest
         return False
+    
+def versionToInt(version: str):
+    return int(''.join([i for i in version if i.isdigit()]))
+    
