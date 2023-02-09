1ba8e420af3dbdccd5366767663cb06eb5bb3e8c
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2023-02-09 13:05:31 +0100

    Fix macOS Disk used percentage

--- IoTuring/Entity/Deployments/Disk/Disk.py
+++ IoTuring/Entity/Deployments/Disk/Disk.py
@@ -8,13 +8,20 @@ KEY_USED_PERCENTAGE = 'space_used_percentage'
 
 class Disk(Entity):
     NAME = "Disk"
+    DEPENDENCIES = ["Os"]
 
     def Initialize(self):
         self.RegisterEntitySensor(EntitySensor(self, KEY_USED_PERCENTAGE))
+
+    def PostInitialize(self):
+        self.os = self.GetDependentEntitySensorValue('Os', "operating_system")
 
     def Update(self):
         self.SetEntitySensorValue(KEY_USED_PERCENTAGE, self.GetDiskUsedPercentage(
         ), ValueFormatter.Options(ValueFormatter.TYPE_PERCENTAGE))
 
     def GetDiskUsedPercentage(self):
-        return psutil.disk_usage('/')[3]
+        if self.os == 'macOS':
+            return psutil.disk_usage('/Users')[3] # macos has a different disk structure
+        else:
+            return psutil.disk_usage('/')[3]
