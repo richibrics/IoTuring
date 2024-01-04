ea4e14a77f50389f0d8da365760ac0bbd5d6b9c4
Jonas Fenchel <locken.kop@web.de>
2024-01-04 15:45:46 +0100

    rearrange fix percent and dbm

--- IoTuring/Entity/Deployments/Wifi/Wifi.py
+++ IoTuring/Entity/Deployments/Wifi/Wifi.py
@@ -1,43 +1,43 @@
+import re
+from socket import AddressFamily
+
+import psutil
+
+from IoTuring.Configurator.MenuPreset import MenuPreset
 from IoTuring.Entity.Entity import Entity
 from IoTuring.Entity.EntityData import EntitySensor
 from IoTuring.Entity.ValueFormat import ValueFormatterOptions
 from IoTuring.MyApp.SystemConsts import OperatingSystemDetection as OsD
 
-
-import psutil
-import re
-
-
 VALUEFORMATTEROPTIONS_MBPS = ValueFormatterOptions(
-    ValueFormatterOptions.TYPE_BIT_PER_SECOND, 
-    adjust_size='Mbps'
-    )
-valueFormatterOptions=ValueFormatterOptions(
-    ValueFormatterOptions.TYPE_RADIOPOWER
-    )
+    ValueFormatterOptions.TYPE_BIT_PER_SECOND, adjust_size="Mbps"
+)
+valueFormatterOptions = ValueFormatterOptions(ValueFormatterOptions.TYPE_RADIOPOWER)
 WIFI_CHOICE_STRING = "Name: {:<15}, IP: {:<16}, MAC: {:<11}"
 
+CONFIG_KEY_WIFI = "wifi"
 
-KEY_SIGNAL_STRENGTH = "signal_strength"
+KEY_SIGNAL_STRENGTH_PERCENT = "signal_strength_percent"
+KEY_SIGNAL_STRENGTH_DBM = "signal_strength_dbm"
 # WINDOWS
-EXTRA_KEY_NAME = 'name'
-EXTRA_KEY_DESCRIPTION = 'description'
-EXTRA_KEY_GUID = 'guid'
-EXTRA_KEY_PHYSICAL_ADDRESS = 'physical_address'
-EXTRA_KEY_STATE = 'state'
-EXTRA_KEY_SSID = 'ssid'
-EXTRA_KEY_BSSID = 'bssid'
-EXTRA_KEY_NETWORK_TYPE = 'network_type'
-EXTRA_KEY_RADIO_TYPE = 'radio_type'
-EXTRA_KEY_AUTHENTICATION = 'authentication'
-EXTRA_KEY_CIPHER = 'cipher'
-EXTRA_KEY_CONNECTION_MODE = 'connection_mode'
-EXTRA_KEY_CHANNEL = 'channel'
-EXTRA_KEY_RECEIVE_RATE = 'receive_rate'
-EXTRA_KEY_TRANSMIT_RATE = 'transmit_rate'
-EXTRA_KEY_SIGNAL = 'signal'
-EXTRA_KEY_PROFILE = 'profile'
-EXTRA_KEY_HOSTED_NETWORK_STATUS = 'hosted_network_status'
+EXTRA_KEY_NAME = "name"
+EXTRA_KEY_DESCRIPTION = "description"
+EXTRA_KEY_GUID = "guid"
+EXTRA_KEY_PHYSICAL_ADDRESS = "physical_address"
+EXTRA_KEY_STATE = "state"
+EXTRA_KEY_SSID = "ssid"
+EXTRA_KEY_BSSID = "bssid"
+EXTRA_KEY_NETWORK_TYPE = "network_type"
+EXTRA_KEY_RADIO_TYPE = "radio_type"
+EXTRA_KEY_AUTHENTICATION = "authentication"
+EXTRA_KEY_CIPHER = "cipher"
+EXTRA_KEY_CONNECTION_MODE = "connection_mode"
+EXTRA_KEY_CHANNEL = "channel"
+EXTRA_KEY_RECEIVE_RATE = "receive_rate"
+EXTRA_KEY_TRANSMIT_RATE = "transmit_rate"
+EXTRA_KEY_SIGNAL = "signal"
+EXTRA_KEY_PROFILE = "profile"
+EXTRA_KEY_HOSTED_NETWORK_STATUS = "hosted_network_status"
 # LINUX
 EXTRA_KEY_ESSID = "ESSID"
 EXTRA_KEY_MODE = "Mode"
@@ -54,20 +54,20 @@ EXTRA_KEY_TX_EXCESSIVE_RETRIES = "Tx_excessive_retries"
 EXTRA_KEY_INVALID_MISC = "Invalid_misc"
 EXTRA_KEY_MISSED_BEACON = "Missed_beacon"
 # MACOS
-EXTRA_KEY_AGRCTLRSSI = 'agrCtlRSSI'
-EXTRA_KEY_AGREXTRSSI = 'agrExtRSSI'
-EXTRA_KEY_AGRCTLNOISE = 'agrCtlNoise'
-EXTRA_KEY_AGREXTNOISE = 'agrExtNoise'
-#EXTRA_KEY_STATE = 'state' # already in windows
-EXTRA_KEY_OP_MODE = 'op mode'
-EXTRA_KEY_LASTTXRATE = 'lastTxRate'
-EXTRA_KEY_MAXRATE = 'maxRate'
-EXTRA_KEY_LASTASSOCSTATUS = 'lastAssocStatus'
-EXTRA_KEY_802_11_AUTH = '802.11 auth'
-EXTRA_KEY_LINK_AUTH = 'link auth'
+EXTRA_KEY_AGRCTLRSSI = "agrCtlRSSI"
+EXTRA_KEY_AGREXTRSSI = "agrExtRSSI"
+EXTRA_KEY_AGRCTLNOISE = "agrCtlNoise"
+EXTRA_KEY_AGREXTNOISE = "agrExtNoise"
+# EXTRA_KEY_STATE = 'state' # already in windows
+EXTRA_KEY_OP_MODE = "op mode"
+EXTRA_KEY_LASTTXRATE = "lastTxRate"
+EXTRA_KEY_MAXRATE = "maxRate"
+EXTRA_KEY_LASTASSOCSTATUS = "lastAssocStatus"
+EXTRA_KEY_802_11_AUTH = "802.11 auth"
+EXTRA_KEY_LINK_AUTH = "link auth"
 # EXTRA_KEY_BSSID = 'BSSID' # already in windows
 # EXTRA_KEY_SSID = 'SSID' # already in windows
-EXTRA_KEY_MCS = 'MCS'
+EXTRA_KEY_MCS = "MCS"
 # EXTRA_KEY_CHANNEL = 'channel' # already in windows
 
 
@@ -75,13 +75,15 @@ class Wifi(Entity):
     NAME = "Wifi"
 
     def Initialize(self):
+        self.wifiInterface = self.GetFromConfigurations(CONFIG_KEY_WIFI)
+
         self.commands = {
             OsD.WINDOWS: ["netsh", "wlan", "show", "interfaces"],
-            OsD.LINUX: ["iwconfig", wifiInterface], # TODO get the wireless interface since os support is only ran during config
-            OsD.MACOS: ["airport", "I"]
+            OsD.LINUX: ["iwconfig", self.wifiInterface],
+            OsD.MACOS: ["airport", "I"],
         }
         self.patterns = {
-            OsD.WINDOWS : {
+            OsD.WINDOWS: {
                 "Description": r"Description\s+:\s+(.*)",
                 "GUID": r"GUID\s+:\s+([\w-]+)",
                 "Physical address": r"Physical address\s+:\s+([\w:]+)",
@@ -98,25 +100,25 @@ class Wifi(Entity):
                 "Transmit rate": r"Transmit rate \(Mbps\)\s+:\s+([\d.]+)",
                 "Signal": r"Signal\s+:\s+(\d+%?)",
                 "Profile": r"Profile\s+:\s+(.*)",
-                "Hosted network status": r"Hosted network status\s+:\s+(.*)"
+                "Hosted network status": r"Hosted network status\s+:\s+(.*)",
             },
-            OsD.LINUX : {
+            OsD.LINUX: {
                 "ESSID": r'ESSID:"([^"]*)"',
-                "Mode": r'Mode:([^\s]+)',
-                "Frequency": r'Frequency:([\d.]+) (GHz|Mhz)',
-                "Access_Point": r'Access Point: ([\w:]+)',
-                "Bit_Rate": r'Bit Rate=([\d.]+) (\w+/s)',
-                "Tx_Power": r'Tx-Power=(-?\d+) (\w+)',
-                "Link_Quality": r'Link Quality=(\d+/\d+)',
-                "Signal_Level": r'Signal level=(-?\d+) (\w+)',
-                "Rx_invalid_nwid": r'Rx invalid nwid:(\d+)',
-                "Rx_invalid_crypt": r'Rx invalid crypt:(\d+)',
-                "Rx_invalid_frag": r'Rx invalid frag:(\d+)',
-                "Tx_excessive_retries": r'Tx excessive retries:(\d+)',
-                "Invalid_misc": r'Invalid misc:(\d+)',
-                "Missed_beacon": r'Missed beacon:(\d+)'
+                "Mode": r"Mode:([^\s]+)",
+                "Frequency": r"Frequency:([\d.]+) (GHz|Mhz)",
+                "Access_Point": r"Access Point: ([\w:]+)",
+                "Bit_Rate": r"Bit Rate=([\d.]+) (\w+/s)",
+                "Tx_Power": r"Tx-Power=(-?\d+) (\w+)",
+                "Link_Quality": r"Link Quality=(\d+/\d+)",
+                "Signal_Level": r"Signal level=(-?\d+) (\w+)",
+                "Rx_invalid_nwid": r"Rx invalid nwid:(\d+)",
+                "Rx_invalid_crypt": r"Rx invalid crypt:(\d+)",
+                "Rx_invalid_frag": r"Rx invalid frag:(\d+)",
+                "Tx_excessive_retries": r"Tx excessive retries:(\d+)",
+                "Invalid_misc": r"Invalid misc:(\d+)",
+                "Missed_beacon": r"Missed beacon:(\d+)",
             },
-            OsD.MACOS : {
+            OsD.MACOS: {
                 "agrCtlRSSI": r"agrCtlRSSI:\s+(-?\d+)",
                 "agrExtRSSI": r"agrExtRSSI:\s+(-?\d+)",
                 "agrCtlNoise": r"agrCtlNoise:\s+(-?\d+)",
@@ -131,54 +133,57 @@ class Wifi(Entity):
                 "BSSID": r"BSSID:\s+([\w:]+)",
                 "SSID": r"SSID:\s+([\w\s]+)",
                 "MCS": r"MCS:\s+(\d+)",
-                "channel": r"channel:\s+([\d,]+)"
-            }
+                "channel": r"channel:\s+([\d,]+)",
+            },
         }
-
         self.platform = OsD.GetOs()
+        if (
+            self.platform == OsD.WINDOWS
+        ):  # Windows shows signal stgrength in percent Unix Systems in dbm
+            self.keySignalStrength = KEY_SIGNAL_STRENGTH_PERCENT
+        else:
+            self.keySignalStrength = KEY_SIGNAL_STRENGTH_DBM
 
         self.RegisterEntitySensor(
             EntitySensor(
                 self,
-                KEY_SIGNAL_STRENGTH,
+                self.keySignalStrength,
                 supportsExtraAttributes=True,
                 valueFormatterOptions=ValueFormatterOptions(
                     ValueFormatterOptions.TYPE_RADIOPOWER
-                )
+                ),
             )
         )
 
     def Update(self):
         p = self.RunCommand(self.commands[self.platform])
         if not p.stdout:
-            raise Exception("error in GetWirelessInfo\n", p.stderr)            
+            raise Exception("error in GetWirelessInfo\n", p.stderr)
         wifiInfo = self.GetWirelessInfo(p.stdout)
         if not wifiInfo:
             raise Exception("error while parsing wirelessInfo")
         # set signal strength
-        if hasattr(wifiInfo, "Signal"): # Windows
+        if hasattr(wifiInfo, "Signal"):  # Windows
             self.SetEntitySensorValue(
-                    key=KEY_SIGNAL_STRENGTH,
-                    value=wifiInfo["Signal"]
-                )
-        elif hasattr(wifiInfo, "Signal Level"): # Linux
+                key=self.keySignalStrength, value=wifiInfo["Signal"]
+            )
+        elif hasattr(wifiInfo, "Signal Level"):  # Linux
             self.SetEntitySensorValue(
-                    key=KEY_SIGNAL_STRENGTH,
-                    value=wifiInfo["Signal Level"]
-                )
+                key=self.keySignalStrength, value=wifiInfo["Signal Level"]
+            )
         elif hasattr(wifiInfo, "agrCtlRSSI"):
             self.SetEntitySensorValue(
-                    key=KEY_SIGNAL_STRENGTH,
-                    value=wifiInfo["agrCtlRSSI"]
-                )
+                key=self.keySignalStrength, value=wifiInfo["agrCtlRSSI"]
+            )
+        else:
+            raise Exception("no Signal Level found")
         for key in self.patterns[self.platform]:
             extraKey = "EXTRA_KEY_" + key.upper().replace(" ", "_")
             self.SetEntitySensorExtraAttribute(
-                sensorDataKey=KEY_SIGNAL_STRENGTH,
+                sensorDataKey=self.keySignalStrength,
                 attributeKey=globals()[extraKey],
-                attributeValue=wifiInfo[key]
+                attributeValue=wifiInfo[key],
             )
-
 
     def GetWirelessInfo(self, stdout):
         wifi_info = {}
@@ -186,48 +191,86 @@ class Wifi(Entity):
             match = re.search(pattern, stdout, re.IGNORECASE)
             if match:
                 wifi_info[key] = match.group(1) if match.group(1) else match.group(0)
-                
+
         self.Log(self.LOG_DEBUG, wifi_info)
         return wifi_info
 
-    def GetWirelessStrength_Mac(self):
-        raise NotImplementedError
+    @classmethod
+    def ConfigurationPreset(cls) -> MenuPreset:
+        NIC_CHOICES = []
+
+        nicip4 = ""
+        nicip6 = ""
+        nicmac = ""
+        interfaces = psutil.net_if_addrs()
+
+        if OsD.IsLinux():
+            for interface in interfaces:
+                p = OsD.RunCommand(["iwconfig", interface])
+                if not (
+                    p.stdout
+                ):  # if there is nothing in stdout, nic is no wireless interface
+                    continue
+                nicinfo = interfaces[interface]  # TODO Typehint
+                for family in nicinfo.family:
+                    if family.family == AddressFamily.AF_INET:
+                        nicip4 = family.address
+                        continue
+                    elif family.family == AddressFamily.AF_INET6:
+                        nicip6 = family.address
+                        continue
+                    elif family.family == psutil.AF_LINK:
+                        nicmac = family.address
+                        continue
+
+                NIC_CHOICES.append(
+                    {
+                        "name": WIFI_CHOICE_STRING.format(
+                            interface,
+                            nicip4 if nicip4 else nicip6,  # defaults to showing ipv4
+                            nicmac,
+                        ),
+                        "value": interface,
+                    }
+                )
+        if OsD.IsWindows():
+            p = OsD.RunCommand(["netsh", "wlan", "show", "interfaces"])
+            # TODO implement
+
+        preset = MenuPreset()
+        choiceAmount = len(NIC_CHOICES)
+        if not choiceAmount:
+            raise Exception("no wireless interfaces found")
+        elif choiceAmount == 1:
+            preset.AddEntry(
+                name="Is This the correct WiFi Interface",
+                key=CONFIG_KEY_WIFI,
+                mandatory=False,
+                question_type="yesno",
+            )
+        else:
+            preset.AddEntry(
+                name="Interface to check",
+                key=CONFIG_KEY_WIFI,
+                mandatory=True,
+                question_type="select",
+                choices=NIC_CHOICES,
+            )
+        return preset
 
     @classmethod
     def CheckSystemSupport(cls):
-        wifiInterface = "" # TODO add this interface to config to get it during init
-
         if OsD.IsLinux():
             if not OsD.CommandExists("iwconfig"):
                 raise Exception("iwconfig not found")
-            
-            sysClassNet = "/sys/class/net/{}/wireless"
-            interfaces = psutil.net_if_addrs()
-
-            import os
-            for interface in interfaces:
-                if os.path.exists(sysClassNet.format(interface)):
-                    wifiInterface = interface
 
         elif OsD.IsWindows():
-            interfaces = psutil.net_if_addrs()
+            if not OsD.CommandExists("netsh"):
+                raise Exception("netsh not found")
 
-            for interface in interfaces:
-                if interface == 'WiFi':
-                    wifiInterface = interface
-        if not interface:
-                raise Exception("no wifi interface found")
-        
         elif OsD.IsMacos():
-            # preferences_file = '/Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist' maybe there is something here
-            # but without cli tools maybe hard to find
-            #
-            # 
             if not OsD.CommandExists("airport"):
                 raise Exception("airport not found")
-            raise Exception("Macos not supported")
-            
-            
-            
+
         else:
             raise Exception("OS detection failed")
