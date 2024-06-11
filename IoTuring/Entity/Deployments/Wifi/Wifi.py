7ba48c3526177b0d001925a02303c88082955f40
Jonas Fenchel <locken.kop@web.de>
2024-06-11 11:10:27 +0200

    refactor for linux, remove windows stuff

--- IoTuring/Entity/Deployments/Wifi/Wifi.py
+++ IoTuring/Entity/Deployments/Wifi/Wifi.py
@@ -25,41 +25,21 @@ SHOW_NA = False  # don't show not available extraAttributes
 KEY_SIGNAL_STRENGTH_PERCENT = "signal_strength_percent"
 KEY_SIGNAL_STRENGTH_DBM = "signal_strength_dbm"
 
-# WINDOWS
+# LINUX
 EXTRA_KEY_NAME = "name"
 EXTRA_KEY_DESCRIPTION = "description"
 EXTRA_KEY_PHYSICAL_ADDRESS = "physical_address"
 EXTRA_KEY_STATE = "state"
+EXTRA_KEY_BSSID = "BSSID"
 EXTRA_KEY_SSID = "ssid"
-EXTRA_KEY_BSSID = "bssid"
-EXTRA_KEY_NETWORK_TYPE = "network_type"
-EXTRA_KEY_RADIO_TYPE = "radio_type"
-EXTRA_KEY_SIGNAL = "signal"
-EXTRA_KEY_PROFILE = "profile"
-EXTRA_KEY_HOSTED_NETWORK_STATUS = "hosted_network_status"
-# LINUX
-# EXTRA_KEY_BSSID = "BSSID" # already in windows
-# EXTRA_KEY_SSID = "ssid" # already in windows
 EXTRA_KEY_FREQUENCY = "Frequency"
-# EXTRA_KEY_RX_BYTES = "RX_bytes"
-# EXTRA_KEY_TX_BYTES = "TX_bytes"
 EXTRA_KEY_SIGNAL = "Signal"
-# EXTRA_KEY_RX_BITRATE = "RX_bitrate"
-# EXTRA_KEY_TX_BITRATE = "TX_bitrate"
-# EXTRA_KEY_BSS_FLAGS = "BSS_flags"
-# EXTRA_KEY_DTIM_PERIOD = "DTIM_period"
-# EXTRA_KEY_BEACON_INTERVAL = "Beacon_interval"
 # MACOS
 EXTRA_KEY_AGRCTLRSSI = "agrCtlRSSI"
 EXTRA_KEY_AGREXTRSSI = "agrExtRSSI"
-# EXTRA_KEY_STATE = 'state' # already in windows
 EXTRA_KEY_OP_MODE = "op mode"
 EXTRA_KEY_LASTTXRATE = "lastTxRate"
 EXTRA_KEY_MAXRATE = "maxRate"
-
-# EXTRA_KEY_BSSID = 'BSSID' # already in windows
-# EXTRA_KEY_SSID = 'SSID' # already in windows
-# EXTRA_KEY_CHANNEL = 'channel' # already in windows
 
 
 class Wifi(Entity):
@@ -76,6 +56,10 @@ class Wifi(Entity):
         if OsD.IsMacos():
             self.language = "en"
 
+        if OsD.IsWindows():
+            # Windows support is WIP https://github.com/richibrics/IoTuring/pull/89
+            raise NotImplementedError
+
         self.wifiInterface = self.GetFromConfigurations(CONFIG_KEY_WIFI)
 
         self.commands = {
@@ -87,43 +71,6 @@ class Wifi(Entity):
             ],
         }
         self.patterns = {
-            OsD.WINDOWS: {
-                #"en": {
-                    "Description": r"Description\s+:\s+(.*)",
-                    "Physical address": r"Physical address\s+:\s+([\w:]+)",
-                    "State": r"State\s+:\s+(.*)",
-                    "SSID": r"SSID\s+:\s+(.*)",
-                    "BSSID": r"BSSID\s+:\s+([\w:]+)",
-                    "Network type": r"Network type\s+:\s+(.*)",
-                    "Radio type": r"Radio type\s+:\s+(.*)",
-                    "Signal": r"Signal\s+:\s+(\d+%?)",
-                    "Profile": r"Profile\s+:\s+(.*)",
-                    "Hosted network status": r"Hosted network status\s+:\s+(.*)",
-                },
-                #"de": {
-                #    "Schnittstellenname": r"Schnittstellenname:\s+(.+)",
-                #    "Beschreibung": r"Beschreibung:\s+(.+)",
-                #    "Physische Adresse": r"Physische Adresse:\s+([0-9A-Fa-f-]+)",
-                #    "Status": r"Status:\s+(.+)",
-                #    "SSID": r"SSID:\s+(.+)",
-                #    "BSSID": r"BSSID:\s+([0-9A-Fa-f-]+)",
-                #    "Netzwerktyp": r"Netzwerktyp:\s+(.+)",
-                #    "Funktyp": r"Funktyp:\s+(.+)",
-                #    "Signal": r"Signal:\s+(\d+%?)",
-                #    "Profil": r"Profil:\s+(.+)",
-                #},
-                #"sp": {
-                #    "Nombre de interfaz": r"Nombre de interfaz:\s+(.+)",
-                #    "Descripción": r"Descripción:\s+(.+)",
-                #    "Dirección física": r"Dirección física:\s+([0-9A-Fa-f-]+)",
-                #    "Estado": r"Estado:\s+(.+)",
-                #    "SSID": r"SSID:\s+(.+)",
-                #    "BSSID": r"BSSID:\s+([0-9A-Fa-f-]+)",
-                #    "Tipo de red": r"Tipo de red:\s+(.+)",
-                #    "Señal": r"Señal:\s+(\d+%?)",
-                #    "Perfil": r"Perfil:\s+(.+)",
-                #},
-            #},
             OsD.LINUX: {
                 "BSSID": r'Connected to (\S+) \(on \S+\)',
                 "SSID": r'SSID: (.+)',
@@ -174,16 +121,6 @@ class Wifi(Entity):
         if not wifiInfo:
             raise Exception("error while parsing wirelessInfo")
         # set signal strength
-        if self.platform == OsD.WINDOWS and "Signal" in wifiInfo:
-            if SIGNAL_UNIT == "%":
-                self.SetEntitySensorValue(
-                    key=self.keySignalStrength, value=wifiInfo["Signal"]
-                )
-            elif SIGNAL_UNIT == "dBm":
-                self.SetEntitySensorValue(
-                    key=self.keySignalStrength,
-                    value=self.PercentToDbm(int(wifiInfo["Signal"][:-1])),
-                )
         elif self.platform == OsD.LINUX and "Signal" in wifiInfo:
             self.SetEntitySensorValue(
                 key=self.keySignalStrength, value=wifiInfo["Signal"]
@@ -222,7 +159,6 @@ class Wifi(Entity):
     @classmethod
     def ConfigurationPreset(cls) -> MenuPreset:
         NIC_CHOICES = Wifi.GetWifiNics(getInfo=True)
-        SIGNAL_CHOICES = ["%", "dBm"]
 
         preset = MenuPreset()
         preset.AddEntry(
@@ -232,19 +168,6 @@ class Wifi(Entity):
             question_type="select",
             choices=NIC_CHOICES,
         )
-        if OsD.IsWindows():
-            preset.AddEntry(
-                name="Signal Quality in dBm or percent",
-                key=SIGNAL_UNIT,
-                mandatory=True,
-                question_type="select",
-                choices=SIGNAL_CHOICES,
-            )
-        return preset
-
-    @staticmethod
-    def PercentToDbm(percentage):
-        return (percentage / 2) - 100
 
     @staticmethod
     def GetWifiNics(getInfo=True):
@@ -364,15 +287,7 @@ class Wifi(Entity):
                 raise Exception("no wireless interface found")
 
         elif OsD.IsWindows():
-            if not OsD.CommandExists("netsh"):
-                raise Exception("netsh not found")
-            elif "English" not in locale.getlocale()[0]:
-                raise Exception(
-                    "locale not supported, create a github issue with the output of 'netsh wlan show interfaces' atached"
-                )
-            wifiNics = Wifi.GetWifiNics(getInfo=False)
-            if not wifiNics:
-                raise Exception("no wireless interface found")
+            raise Exception("Windows is not supported at the moment")
 
         elif OsD.IsMacos():
             if not OsD.CommandExists("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"):
